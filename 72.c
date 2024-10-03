f7151b9f537347c9c0e945e993969c294d82c4f2
Aditya01i <heenaadi07@gmail.com>
2024-10-03 15:40:14 +0530

    Add files via upload

---
+++ 72.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+void main()
+{
+    int num;
+    printf("Enter the number of elements you want inn the array ");
+    scanf("%d", &num);
+    printf("Enter the elements ");
+    int a[num];
+    for (int i = 0; i < num; i++)
+    {
+        scanf("%d", &a[i]);
+    }
+    int rotate_point;
+    printf("Enter the number of positions you want to rotate by ");
+    scanf("%d", &rotate_point);
+    int b[num];
+    for (int i = 0; i < num; i++)
+    {
+        if (i + rotate_point < num)
+        {
+            b[i + rotate_point] = a[i];
+        }
+        if (i + rotate_point >= num)
+        {
+            b[i + rotate_point - num] = a[i];
+        }
+    }
+    printf("The array after rotation is : \n");
+    for (int i = 0; i < num; i++)
+    {
+        printf("%d ", b[i]);
+    }
+}
---
+++ 73.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+void main()
+{
+    int a[6], c;
+    for (int i = 0; i < 6; i++)
+    {
+        a[i] = 0;
+    }
+    printf("Enter the number of votes ");
+    int voters;
+    scanf("%d", &voters);
+    for (int i = 1; i <= voters; i++)
+    {
+        printf("Enter your choice of candidates between 0 to 5 : ");
+        scanf("%d", &c);
+        switch (c)
+        {
+        case 1:
+            a[0]++;
+            break;
+        case 2:
+            a[1]++;
+            break;
+        case 3:
+            a[2]++;
+            break;
+        case 4:
+            a[3]++;
+            break;
+        case 5:
+            a[4]++;
+            break;
+        default:
+            a[5]++;
+            break;
+        }
+    }
+    printf("No. of votes for candidate 1 : %d \n", a[0]);
+    printf("No. of votes for candidate 2 : %d \n", a[1]);
+    printf("No. of votes for candidate 3 : %d \n", a[2]);
+    printf("No. of votes for candidate 4 : %d \n", a[3]);
+    printf("No. of votes for candidate 5 : %d \n", a[4]);
+    printf("SPOILT BALLOTS  : %d ", a[5]);
+}
